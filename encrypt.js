c6ca64df568191f47c8d3d9baea75c56bb25eed4
tavern <66744142+TSVRN9@users.noreply.github.com>
2022-12-18 09:04:13 -0500

    PWA capabilities

--- encrypt.js
+++ encrypt.js
@@ -3,25 +3,45 @@
 const CryptoJS = require("crypto-js");
 const fs = require('fs');
 const path = require('path');
-const terser = require('terser');
+const colors = require('colors');
 const { password } = require('./config.json');
 
 const directory = path.join(__dirname, 'unencrypted');
 const encryptedDirectory = path.join(__dirname, 'dist', 'encrypted')
 
-const files = fs.readdirSync(directory);
+// if (!fs.existsSync(path.join(__dirname, 'dist'))) fs.mkdirSync(path.join(__dirname, 'dist'));
+if (!fs.existsSync(encryptedDirectory)) fs.mkdirSync(encryptedDirectory, { recursive: true });
 
-files.forEach(async filename => {
-        const filepath = path.join(directory, filename);
-        const contents = fs.readFileSync(filepath, { encoding: 'UTF-8' });
+console.log();
+console.log(`Attempting to encrypt directory: ${directory.toString().cyan} to ${encryptedDirectory.toString().cyan}`);
+
+const files = 
+    Promise.all(fs.readdirSync(directory)
+        .map(async filename => {
+            const filepath = path.join(directory, filename);
+            const contents = fs.readFileSync(filepath, { encoding: 'UTF-8' });
+
+            const data = require(filepath);
+            const { ciphertext, salt, iv } = await CryptoJS.AES.encrypt(JSON.stringify(data, null, null), password);
+
+            // write to ./dist
+            // .js to .json
+            const encryptedFilename = filename.substring(0, filename.length - '.js'.length) + '.json';
+            const encryptedpath = path.join(encryptedDirectory, encryptedFilename);
         
-        eval(contents); // read a js object
-        const { ciphertext, salt } = await CryptoJS.AES.encrypt(JSON.stringify(code, null, null), password);
-
-        // write to ./dist
-        // .js to .json
-        const encryptedpath = path.join(encryptedDirectory, filename.substring(0, filename.length - '.js'.length) + '.json');
-        fs.writeFileSync(encryptedpath, JSON.stringify({ ciphertext, salt }, null, null));
-    });
-
-fs.writeFileSync(path.join(encryptedDirectory, 'data.json'), JSON.stringify(files, null, null));
+            fs.writeFileSync(encryptedpath, 
+                JSON.stringify({ 
+                    ciphertext: ciphertext.toString(CryptoJS.enc.Base64), 
+                    salt: salt.toString(CryptoJS.enc.Base64),
+                    iv: iv.toString(CryptoJS.enc.Base64), 
+                }, null, null));
+        
+            console.log(`Encrypted ${filename.cyan} to ${encryptedFilename.toString().cyan}`);
+        
+            return encryptedFilename;
+        }))
+        .then(f => {
+            fs.writeFileSync(path.join(encryptedDirectory, 'data.json'), JSON.stringify(f, null, null));
+            console.log(`Created data.json`.cyan);
+            console.log();
+        });
--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
+const CryptoJS = require('crypto-js');
+
 async function decrypt(password, filename) {
-    const encryptedFile = await axios.get(`encrypted/${filename}`).data;
-    const { ciphertext, salt } = JSON.parse(encryptedFile);
-    const decrypted = CryptoJS.AES.decrypt({ ciphertext, salt });
-    return JSON.parse(decrypted);
+    const { ciphertext, salt, iv } = (await axios.get(`./encrypted/${filename}`)).data;
+    const encrypted = CryptoJS.lib.CipherParams.create({
+        ciphertext: CryptoJS.enc.Base64.parse(ciphertext),
+        salt: CryptoJS.enc.Base64.parse(salt),
+        iv: CryptoJS.enc.Base64.parse(iv),
+    })
+    console.log(encrypted);
+    const decrypted = CryptoJS.AES.decrypt(encrypted, password);
+    return JSON.parse(decrypted.toString(CryptoJS.enc.Utf8));
 }
+
+async function registerServiceWorker() {
+    if ('serviceWorker' in navigator) {
+        try {
+            // trick to get parcel to not compile service-worker.js
+            const filename = 'service-worker.js';
+            const url = new URL(filename, import.meta.url);
+            const registration = await navigator.serviceWorker.register(url);
+
+            if (registration.installing) {
+              console.log("Service worker installing");
+            } else if (registration.waiting) {
+              console.log("Service worker installed");
+            } else if (registration.active) {
+              console.log("Service worker active");
+            }
+        } catch (e) {
+            console.error(e);
+        }
+    }
+}
+
+window.on('load', () => {
+    registerServiceWorker();
+});
